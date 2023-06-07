3ab7b66c5281be669b07e09cf58dfd914d6ab5f2
Mauricio Sotelo <mautsotelo@gmail.com>
2023-06-07 20:41:19 -0300

    feat: Creating Avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
-import { PencilLine } from "phosphor-react"
+import { PencilLine } from "phosphor-react";
+import { Avatar } from './Avatar';
 
 import styles from "./Sidebar.module.css";
 
@@ -10,8 +11,8 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1514760263916-4ac83991b2e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
-        <img 
-          className={styles.avatar}
+        
+        <Avatar
           src="https://github.com/mautsotelo.png"
         />
 
