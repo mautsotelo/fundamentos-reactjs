fe7b71e8bdca73ee5f8a08564cc591f122959aeb
Mauricio Sotelo <mautsotelo@gmail.com>
2023-06-06 22:57:04 -0300

    feat: inital commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
+
+import "./global.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
